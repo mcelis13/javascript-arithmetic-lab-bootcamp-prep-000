43acc14beda355ec3fc4078bb68fb704aae58315
Madeline Celis <madelinebcelis@gmail.com>
2018-07-15 21:04:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+  return num1 + num2;
+};
+
+function subtract(a, b){
+  return num1 - num2;
+};
+
+function divide(a, b){
+  return num1/num2;
+};
+
+function multiply(a, b){
+  return num1 * num2;
+};
+
+function inc(a){
+  return num++;
+}
+
+function dec(a){
+  return num--;
+}
+
+function makeInt(string){
+  return Number(string);
+}
